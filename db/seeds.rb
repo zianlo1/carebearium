6506454d2544adae181737081f1da753eb83034d
Toms Mikoss <toms.mikoss@gmail.com>
2014-08-24 18:23:46 +0300

    Load data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+def read_file(filename)
+  MultiJson.load File.read(Rails.root.join 'db', 'seeds', filename), symbolize_keys: true
+end
+
+SolarSystem.transaction do
+  p 'Loading solar systems'
+  read_file('solar_systems.json').each do |row|
+    SolarSystem.where(id: row[:id]).first_or_initialize.update_attributes(
+      name:         row[:name],
+      region_name:  row[:regionName],
+      security:     row[:security],
+      belt_count:   row[:beltCount]
+    )
+  end
+end
+
+Station.transaction do
+  p 'Loading stations'
+  read_file('stations.json').each do |row|
+    Station.where(id: row[:id]).first_or_initialize.update_attributes(
+      solar_system_id:  row[:solarSystemID],
+      name:             row[:name]
+    )
+  end
+end
+
+Corporation.transaction do
+  p 'Loading corporations'
+  read_file('corporations.json').each do |row|
+    Corporation.where(id: row[:id]).first_or_initialize.update_attributes(
+      name: row[:name]
+    )
+  end
+end
+
+Agent.transaction do
+  p 'Loading agents'
+  read_file('agents.json').each do |row|
+    Agent.where(id: row[:id]).first_or_initialize.update_attributes(
+      corporation_id:   row[:corporationID],
+      station_id:       row[:stationID],
+      level:            row[:level],
+      kind:             row[:kind],
+      solar_system_id:  row[:solarSystemID],
+      locator:          row[:isLocator] == 1
+    )
+  end
+end
