f1b9e23f099b194506c6921b6e7081a51dd563e7
Toms Mikoss <toms.mikoss@gmail.com>
2014-10-07 20:51:06 +0300

    Seeds rebuild solar systems

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,13 @@ def read_file(filename)
 end
 
 p 'Loading solar systems'
+SolarSystem.delete_all
 read_file('solar_systems.json').each do |row|
   SolarSystem.find_or_create_by(id: row[:id])
 end
+
+p 'Updating industry stats'
+SolarSystem.update_industry_indices
+
+p 'Updating kill stats'
+SolarSystem.update_kill_stats
