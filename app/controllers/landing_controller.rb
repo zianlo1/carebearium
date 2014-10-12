49193f91f7aef9761b775c4cc1e6c92249ebc890
Toms Mikoss <toms.mikoss@gmail.com>
2014-10-12 18:42:01 +0300

    Look at ApiLog updates for timestamps

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,6 @@
 class LandingController < ApplicationController
   def show
-    last_update = SolarSystem.max(:updated_at)
+    last_update = ApiLog.max(:called_at)
 
     @solar_system_owner_names = Rails.cache.fetch "solar_system_owner_names/#{last_update.to_i}" do
       SolarSystemOwner.names.to_json
