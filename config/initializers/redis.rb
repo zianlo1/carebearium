64b90b807ac43f4844b409c5fa12b1a8f981f9ad
Toms Mikoss <toms.mikoss@gmail.com>
2014-10-15 11:25:26 +0300

    Did not work correctly

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,8 +2,3 @@ redis_url = ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/0/carebearium"
 
 Carebearium::Application.config.cache_store = :redis_store, redis_url
 Carebearium::Application.config.session_store :redis_store, redis_server: redis_url
-
-Carebearium::Application.config.action_dispatch.rack_cache = {
-  metastore:   "#{redis_url}/metastore",
-  entitystore: "#{redis_url}/entitystore"
-}
