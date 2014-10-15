591075bdacb6172fbfea260790f9f40c98f187e4
Toms Mikoss <toms.mikoss@gmail.com>
2014-10-15 11:48:14 +0300

    seems to work

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,20 +3,17 @@ class LandingController < ApplicationController
 
   def show
     last_update = ApiLog.last_significant_update
-    cache_key   = "landing#show/#{last_update.to_i}"
 
-    expires_in 10.minutes, public: true
+    expires_in 1.hour, public: true
 
-    if stale?(last_modified: last_update, etag: cache_key, public: true)
-      @solar_system_owner_names = Rails.cache.fetch "solar_system_owner_names/#{last_update.to_i}" do
-        SolarSystemOwner.names.to_json
-      end
-
-      @solar_system_limits = Rails.cache.fetch "solar_system_limits/#{last_update.to_i}" do
-        SolarSystem.limits.to_json
-      end
-
-      render text: '', layout: 'application'
+    @solar_system_owner_names = Rails.cache.fetch "solar_system_owner_names/#{last_update.to_i}" do
+      SolarSystemOwner.names.to_json
     end
+
+    @solar_system_limits = Rails.cache.fetch "solar_system_limits/#{last_update.to_i}" do
+      SolarSystem.limits.to_json
+    end
+
+    render text: '', layout: 'application'
   end
 end
